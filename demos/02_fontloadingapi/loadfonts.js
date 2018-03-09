272425fc5170244207b8fb38abe8483c799787fc
fpapado <fotis@futurice.com>
2018-03-09 02:35:05 +0200

    Update 02, add notes

--- demos/02_fontloadingapi/loadfonts.js
+++ demos/02_fontloadingapi/loadfonts.js
@@ -1,47 +1,23 @@
 'use strict';
+
 (function() {
-  if (sessionStorage.fontsLoaded) {
-    document.documentElement.classList.remove('fonts-loading');
-    document.documentElement.classList.add('fonts-active');
-    return;
-  } else {
-    if ('fonts' in document) {
-      loadFonts([
-        document.fonts.load('1em NotoBold'),
-        document.fonts.load('1em Charter'),
-        document.fonts.load('1em Noto')
-      ]);
-    } else {
-      loadScript('/js/fontfaceobserver.js', function() {
-        var notobold = new FontFaceObserver('NotoBold');
-        var charter = new FontFaceObserver('Charter');
-        var noto = new FontFaceObserver('Noto');
-        loadFonts(notobold.load(), charter.load(), noto.load());
+  // Only act if the Font Loading API is supported
+  // TODO: FontFaceObserver
+  // TODO: localStorage
+  if ('fonts' in document) {
+    Promise
+      .all([
+        document.fonts.load('700 1em Lato'),
+        document.fonts.load('1em Noto Serif'),
+      ])
+      .then(function() {
+        document.documentElement.classList.add('fonts-active');
+        sessionStorage.fontsLoaded = true;
+      })
+      .catch(function(err) {
+        // Debatable whether you want this
+        console.error("Fonts could not be loaded ", err);
+        document.documentElement.classList.add('fonts-inactive');
       });
-    }
-    function loadScript(src, done) {
-      var js = document.createElement('script');
-      js.src = src;
-      js.onload = function() {
-        done();
-      };
-      js.onerror = function() {
-        done(new Error('Failed to load script ' + src));
-      };
-      document.head.appendChild(js);
-    }
-    function loadFonts(fontPromises) {
-      Promise.all(fontPromises)
-        .then(function() {
-          document.documentElement.classList.remove('fonts-loading');
-          document.documentElement.classList.add('fonts-active');
-          sessionStorage.fontsLoaded = true;
-        })
-        .catch(function(err) {
-          console.error(err);
-          document.documentElement.classList.remove('fonts-loading');
-          document.documentElement.classList.add('fonts-inactive');
-        });
-    }
   }
 })();
